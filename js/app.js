6e16c70e4becf320f1e80c7411c1d41028ab6563
Luis Furtado <lucho_l.f@hotmail.com>
2023-10-02 16:30:42 -0300

    mostrando imagenes

--- js/app.js
+++ js/app.js
@@ -15,6 +15,9 @@ function validarFormulario(e) {
         mostrarAlerta('Agregar un término de búsqueda');
         return
     }
+
+    // Buscar imagenes
+    buscarImagenes(terminoBusqueda);
 }
 
 function mostrarAlerta(mensaje) {
@@ -36,4 +39,47 @@ function mostrarAlerta(mensaje) {
             alerta.remove()
         }, 3000);
     }
+}
+
+function buscarImagenes(termino) {
+    const key = '39775550-3d85f4d910383e3941994d601';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
+
+    fetch(url) 
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            mostrarImagenes(resultado.hits);
+        })
+}
+
+function mostrarImagenes(imagenes) {
+    
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild)
+    }
+
+    // Iterar sobre el arreglo de imagenes y construir el HTML
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+
+                    <div class="p-4">
+                        <p class="font-bold"> ${likes} <span class="font-light"> Me gusta </span> </p>
+                        <p class="font-bold"> ${views} <span class="font-light"> Veces Vistas </span> </p>
+
+                        <a 
+                            class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer"
+                        >
+                            Ver Imagen
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+    })
 }
